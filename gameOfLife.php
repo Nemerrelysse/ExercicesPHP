3886b584ad6352e7818356ba115f679ff42a4eb1
Nemerrelysse <115214198+Nemerrelysse@users.noreply.github.com>
2023-01-20 17:14:34 +0100

    Update gameOfLife.php

--- gameOfLife.php
+++ gameOfLife.php
@@ -6,28 +6,26 @@
     echo " ";
     echo $value ? "o" : "x";
   }
-  echo "\n";
-}
+    echo "\n";
+  }
 
-$state = [];
-for($i=0 ; $i<L; $i++){
-  $state[$i] = (bool) rand(0,1); 
-}
-print_state($state);
+  $state = [];
+
+  for($i=0 ; $i<L; $i++){
+    $state[$i] = (bool) rand(0,1); 
+  }
+
+  print_state($state);
 
 
-  
-
-  
-  
   for($j=0 ; $j<R; $j++){
     
     $newLine=$state;
-    for($i=0 ; $i<L; $i++){
-      $newLine[$i]=$state[($i-  1+L)%L]!=$state[($i+1+L)%L];
-    }
+      for($i=0 ; $i<L; $i++){
+        $newLine[$i]=$state[($i-  1+L)%L]!=$state[($i+1+L)%L];
+      }
     $state=$newLine;
     print_state($state);
-  }
+    }
   
   ?>
